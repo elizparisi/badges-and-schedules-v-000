eff7d619e680fe2d463e573d042b518d85266d34
Elizabeth Parisi <parisielizabeth@gmail.com>
2018-12-20 23:16:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ def batch_badge_creator(speakers)
 end
 
 def assign_rooms(speakers)
-  speakers.each_with_index.map do |speaker, index|
+  speakers.each_with_index.map do |speakers, index|
     "Hello, #{speakers}! You'll be assigned to room #{index+1}!"
   end
 end
