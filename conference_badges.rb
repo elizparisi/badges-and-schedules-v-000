3e7f24cec9322fd635ba24a991b38ed8196882bf
Elizabeth Parisi <parisielizabeth@gmail.com>
2018-12-20 23:18:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,10 @@
+  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(speakers)
-  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   speakers.map do |speakers|
     "Hello, my name is #{speakers}."
   end
